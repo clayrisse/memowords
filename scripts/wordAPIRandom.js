d17482807d684d1519004e568aa2056e7a09b7c6
clayrisse <clayrisse@outlook.com>
2020-11-02 00:24:05 +0100

    d2 - wAPItest.js create alternative with promise.all and  IT WORKS

--- scripts/wordAPIRandom.js
+++ scripts/wordAPIRandom.js
@@ -35,122 +35,122 @@ function getRandomWordFromApi() {
 
 
 
-// let memoWords = []
-// let fillOrder = []
-// let numberOrder = []
-// let shuffleOrder = []
-// let cardArr = []
-// let boardSize = 4
+let memoWords = []
+let fillOrder = []
+let numberOrder = []
+let shuffleOrder = []
+let cardArr = []
+let boardSize = 4
 
-// const testSection = document.querySelector("#test");
-// getRandomIndex = (arr) => Math.floor(Math.random()*arr.length)
+const testSection = document.querySelector("#test");
+getRandomIndex = (arr) => Math.floor(Math.random()*arr.length)
 
-// for (let i = 1; i<=boardSize; i++) {
-//     cardArr.push("","")
-//     numberOrder.push((i*2)-2, (i*2)-1)
-// }
-// // console.log(numberOrder, shuffleOrder);
+for (let i = 1; i<=boardSize; i++) {
+    cardArr.push("","")
+    numberOrder.push((i*2)-2, (i*2)-1)
+}
+// console.log(numberOrder, shuffleOrder);
 
-// function shuffle() {
-//     for (let i = 1; i<=boardSize; i++) {
-//         let indexOut = getRandomIndex(numberOrder)
-//         shuffleOrder.push(numberOrder[indexOut])
-//         numberOrder.splice(indexOut,1)
-//         // console.log("spliced", numberOrder, "shuffleOrder", shuffleOrder);
-//     }     
-// }
+function shuffle() {
+    for (let i = 1; i<=boardSize; i++) {
+        let indexOut = getRandomIndex(numberOrder)
+        shuffleOrder.push(numberOrder[indexOut])
+        numberOrder.splice(indexOut,1)
+        // console.log("spliced", numberOrder, "shuffleOrder", shuffleOrder);
+    }     
+}
 
-// function getBoradStarted() {
-//     for (let i = 1; i<=boardSize; i++) {
-//         const divTest = document.createElement('div');
-//         divTest.setAttribute("data-key", `${i-1}`)
-//         divTest.setAttribute("class", `wordbox`)
-//         divTest.innerHTML = `<p class="wordtest" data-key="w${i-1}">hello${i-1}</p>`
+function getBoradStarted() {
+    for (let i = 1; i<=boardSize; i++) {
+        const divTest = document.createElement('div');
+        divTest.setAttribute("data-key", `${i-1}`)
+        divTest.setAttribute("class", `wordbox`)
+        divTest.innerHTML = `<p class="wordtest" data-key="w${i-1}">hello${i-1}</p>`
         
-//         // <p id="wordtest" data-key="${i*2-1}">hello${i*2-1}</p>`;
+        // <p id="wordtest" data-key="${i*2-1}">hello${i*2-1}</p>`;
                 
-//         console.log(divTest)
-//         testSection.appendChild(divTest)
-//     }
-// }
-// getBoradStarted()
-// shuffle()
+        console.log(divTest)
+        testSection.appendChild(divTest)
+    }
+}
+getBoradStarted()
+shuffle()
 
 
-// function getWordFromApi() {    
-//     // console.log(testSection)
-//     let wordListRaw = wD1.split("\n")
-//     let wordListClean = wordListRaw.map(x=>x.split(" ")[0])
+function getWordFromApi() {    
+    // console.log(testSection)
+    let wordListRaw = wD1.split("\n")
+    let wordListClean = wordListRaw.map(x=>x.split(" ")[0])
   
-//     for (let i = 1; i <= boardSize/4; i++){
-//     let randomIndex = getRandomIndex(wordListClean)
-//     // console.log("wordFunction-------------->", wordListClean[randomIndex])
+    for (let i = 1; i <= boardSize/4; i++){
+    let randomIndex = getRandomIndex(wordListClean)
+    // console.log("wordFunction-------------->", wordListClean[randomIndex])
        
-//         fetch(`https://wordsapiv1.p.rapidapi.com/words/${wordListClean[randomIndex]}`, {
-//             "method": "GET",
-//             "headers": {
-//             "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-//             "x-rapidapi-key": "e5347b3dabmsh48cbe08706d1f47p19e89ajsne6f06070bcd7"
-//             }
-//         })
-//         .then(response => { 
-//             return response.json(); // response.json() es tambien una operacion asincrona
-//         })
-//         .then((data) => {
-//             let randomePick = getRandomIndex(data.results);
-//             // console.log("round-->",i , "word-->", data.word, "randomIndex-->",randomePick);
-//             // let newWord = {
-//             //     word: data.word,
-//             //     definition: data.results[randomePick].definition,
-//             //     // partOfSpeech: data.results[randomePick].partOfSpeech,
-//             //     // typeOf: data.results[randomePick].typeOf,
-//             //     // synonyms: data.results[randomePick].synonyms
-//             // }
-//             // memoWords.push(newWord) 
-//             // fillOrder.push([
-//             //     i, "word", data.word
-//             // ], [
-//             //     i, "result", 
-//             //     data.results[randomePick].definition, 
-//             //     data.results[randomePick].partOfSpeech
-//             //     // data.results[randomePick].typeOf,
-//             //     // data.results[randomePick].synonyms
-//             // ])
-//             // const articleTest = document.createElement('article');
-//             // articleTest.innerHTML = `<h1 id="wordtest">${data.word}</h1>`;
+        fetch(`https://wordsapiv1.p.rapidapi.com/words/${wordListClean[randomIndex]}`, {
+            "method": "GET",
+            "headers": {
+            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+            "x-rapidapi-key": "e5347b3dabmsh48cbe08706d1f47p19e89ajsne6f06070bcd7"
+            }
+        })
+        .then(response => { 
+            return response.json(); // response.json() es tambien una operacion asincrona
+        })
+        .then((data) => {
+            let randomePick = getRandomIndex(data.results);
+            // console.log("round-->",i , "word-->", data.word, "randomIndex-->",randomePick);
+            // let newWord = {
+            //     word: data.word,
+            //     definition: data.results[randomePick].definition,
+            //     // partOfSpeech: data.results[randomePick].partOfSpeech,
+            //     // typeOf: data.results[randomePick].typeOf,
+            //     // synonyms: data.results[randomePick].synonyms
+            // }
+            // memoWords.push(newWord) 
+            // fillOrder.push([
+            //     i, "word", data.word
+            // ], [
+            //     i, "result", 
+            //     data.results[randomePick].definition, 
+            //     data.results[randomePick].partOfSpeech
+            //     // data.results[randomePick].typeOf,
+            //     // data.results[randomePick].synonyms
+            // ])
+            // const articleTest = document.createElement('article');
+            // articleTest.innerHTML = `<h1 id="wordtest">${data.word}</h1>`;
                     
-//             // console.log(articleTest)
-//             // testSection.appendChild(articleTest)
-//             console.log(shuffleOrder)
-//             console.log(shuffleOrder[i*2-2])
+            // console.log(articleTest)
+            // testSection.appendChild(articleTest)
+            console.log(shuffleOrder)
+            console.log(shuffleOrder[i*2-2])
 
-//             const keySection1 = document.querySelector(`[data-key="${shuffleOrder[i*2-2]}"]`);
-//             const keySection2 = document.querySelector(`[data-key="${shuffleOrder[i*2-1]}"]`);
-//             console.log(keySection1)
-//             console.log(keySection2)
-//             keySection1.innerHTML = `<p class="wordtest word" color="blue">${data.word}</p>`;
-//             keySection1.innerHTML = `<p class="wordtest def">${data.results[randomePick].definition}</p>`;
-//             // textSection1.appendChild(keySection1)
+            const keySection1 = document.querySelector(`[data-key="${shuffleOrder[i*2-2]}"]`);
+            const keySection2 = document.querySelector(`[data-key="${shuffleOrder[i*2-1]}"]`);
+            console.log(keySection1)
+            console.log(keySection2)
+            keySection1.innerHTML = `<p class="wordtest word" color="blue">${data.word}</p>`;
+            keySection1.innerHTML = `<p class="wordtest def">${data.results[randomePick].definition}</p>`;
+            // textSection1.appendChild(keySection1)
         
-//         })
+        })
       
-//         .catch(err => {
-//             console.log(err);
-//         });        
-//     }
+        .catch(err => {
+            console.log(err);
+        });        
+    }
         
-//                             // // -----------------------why does this throws undifine?????
-//                             // for (let k = 0; k < shuffleOrder.length; k++) {
-//                             //     console.log(k, fillOrder, shuffleOrder )
-//                             //     cardArr[shuffleOrder[k]]=fillOrder[k]
-//                             // }
-//                             // console.log(cardArr)
-//                             // //------------------------whyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+                            // -----------------------why does this throws undifine?????
+                            for (let k = 0; k < shuffleOrder.length; k++) {
+                                console.log(k, fillOrder, shuffleOrder )
+                                cardArr[shuffleOrder[k]]=fillOrder[k]
+                            }
+                            console.log(cardArr)
+                            //------------------------whyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
 
-//     // console.log(`BIG ARRAY--->`,memoWords[1], memoWords); //WHY IS UNDEFINED
-//     // nemoWords.forEach(elem => fillOrder.push(elem.word, elem.results[randomePick].definition))
-//     // console.log("fillOrder", fillOrder)
-// };
+    // console.log(`BIG ARRAY--->`,memoWords[1], memoWords); //WHY IS UNDEFINED
+    // nemoWords.forEach(elem => fillOrder.push(elem.word, elem.results[randomePick].definition))
+    // console.log("fillOrder", fillOrder)
+};
 
 // getWordFromApi();
  
--- scripts/wordsAPItest.js
+++ scripts/wordsAPItest.js
@@ -6,12 +6,10 @@ let cardArr = []
 let levelChoise = ""
 let boardSize = 0
 
-// console.log("values-->",levelChoise, boardSizeImput)
-
 const boardSection = document.querySelector("#board");
 getRandomIndex = (arr) => Math.floor(Math.random()*arr.length)
 
-function getArraysSetUp() { //creates 1arr with empty positions & a 2arr with numbers to be shuffle 
+function getArraysSetUp() { //creates 1arr with empty positions & a 2arr with numbers to be shuffle  
     for (let i = 1; i<=boardSize/2; i++) {
         cardArr.push("","")
         numberOrder.push((i*2)-2, (i*2)-1)
@@ -52,10 +50,10 @@ function printShuffleOrderTesting() { //NOT TO USE.
         
         word.setAttribute("data-word", `${(i*2)-2}`)
         word.setAttribute("class", `textword id${i}`)
-        word.setAttribute("value", `${i}`)
+        // word.setAttribute("value", `${i}`)
         definition.setAttribute("data-def", `${(i*2)-1}`)
         definition.setAttribute("class", `textdefinition`)
-        definition.setAttribute("value", `${i}`)
+        // definition.setAttribute("value", `${i}`)
         
         word.textContent = `pipi${(i*2)-2}`;
         definition.textContent = `pipi${(i*2)-1}`;
@@ -63,7 +61,6 @@ function printShuffleOrderTesting() { //NOT TO USE.
         keyWord.appendChild(word) 
         keyDefi.appendChild(definition) 
         console.log(keyWord, keyDefi)
-        // console.log(keyWord)
         // console.log(word.data-word)
 
         keyWord.addEventListener('click', audioTesting)
@@ -73,62 +70,53 @@ function printShuffleOrderTesting() { //NOT TO USE.
 }
 // printShuffleOrderTesting() // PRENDER SOLO PARA HACER TEST DEL LOOP EN EL FETCH
 
-function getWordFromApi() {    
-    // console.log(levelChoise)
-    // console.log(boardSection)    //---------------why does not work ???????????????
-    // boardSection.innerHTML = ""
-
+let wordsArr = []
+function makeArrOfWordsToFetch(levelChoise) {
     let wordListRaw = levelChoise.split("\n")    
     let wordListClean = wordListRaw.map(x=>x.split(" ")[0])
-    // console.log(wordListClean)
-   
-    for (let i = 1; i <= boardSize/2; i++){   //----- LOOP to get as many words user set on inputsd
-    let randomIndex = getRandomIndex(wordListClean)
-       
-        fetch(`https://wordsapiv1.p.rapidapi.com/words/${wordListClean[randomIndex]}`, {
-            "method": "GET",
-            "headers": {
-            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-            "x-rapidapi-key": "e5347b3dabmsh48cbe08706d1f47p19e89ajsne6f06070bcd7"
-            }
-        })
-        .then(response => { 
-            return response.json(); // response.json() es tambien una operacion asincrona
-        })
-        .then((data) => {
-            // console.log(data)
-            let randomePick = getRandomIndex(data.results);
-            
-            //-------selection creation and appendind of elements or words and definition
-            const keyWord = document.querySelector(`[data-key="${shuffleOrder[i*2-2]}"]`);
-            const keyDefi = document.querySelector(`[data-key="${shuffleOrder[i*2-1]}"]`);
-            
-            const word = document.createElement('p');
-            const definition = document.createElement('p');
-            
-            word.setAttribute("data-word", `${(i*2)-2}`)
-            word.setAttribute("class", `textword`)
-            definition.setAttribute("data-def", `${(i*2)-1}`)
-            definition.setAttribute("class", `textdefinition`)
-            
-            word.textContent = `word: ${data.word}`;
-            definition.textContent = `definition: ${data.results[randomePick].definition}`;
-            
-            keyWord.appendChild(word) 
-            keyDefi.appendChild(definition)         
-            
-            keyWord.addEventListener('click', audioTesting)
-            keyDefi.addEventListener('click', audioTesting)
-
-        })
-        // .then(memoWords => {
-        //     console.log("memoWords", memoWords.word)
-        // })
-        .catch(err => {
-            console.log(err);
-        });        
+ 
+    for (let i = 1; i <= boardSize/2; i++) {
+        const randomWord = wordListClean[getRandomIndex(wordListClean)]
+        wordsArr.push(randomWord)
     }
-    
+    return wordsArr
+}
+
+function getWordFromApi() {    
+       
+    let requests = wordsArr.map(word => fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
+        "method": "GET",
+        "headers": {
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+        "x-rapidapi-key": "e5347b3dabmsh48cbe08706d1f47p19e89ajsne6f06070bcd7"
+        }
+    }))
+
+    Promise.all(requests)
+    .then(responses => { // all responses are resolved successfully
+        for (let response of responses) {
+        // alert(`${response.url}: ${response.status}`); 
+        }
+        return responses;
+    }) // map array of responses into an array of response.json() to read their content
+    .then(responses => Promise.all(responses.map(res => {
+        console.log(res)
+        return res.json()
+    }))) // all JSON answers are parsed: "users" is the array of them
+    .then(wordInfo => wordInfo.forEach(wordObj => {
+        console.log("word:", wordObj.word)
+        memoWords.push(wordObj)
+        return memoWords
+        // return alert(word.name)
+    }))
+    .then(arr=>{
+        console.log("arr--------", arr)
+        console.log("memowords--", memoWords[0])
+    })
+    .catch(err => {
+        console.log(err);
+    }); 
+
 };
 
 function setSetings() {
@@ -145,17 +133,14 @@ function setSetings() {
         case "wD1": levelChoise = level.wD1; break;
         }
 
-    // console.log("boardSize", boardSize)
-    // console.log("values------>", boardSizeImput, levelChoiseImput)
-    // console.log(typeof levelChoiseImput)
-    // console.log("values------>",boardSize,)
-    // console.log("values------>", levelChoise)
-
     getArraysSetUp();   // console.log("cardArr:", cardArr, "numberOrder:",numberOrder);
     shuffle();          // console.log("shuffleOrder:",shuffleOrder)
     getBoardStarted();  // console.log(boardSection)
 
-    printShuffleOrderTesting() // PRENDER SOLO PARA HACER TEST DEL LOOP EN EL FETCH
+    makeArrOfWordsToFetch(levelChoise)
+    console.log("wordsArr", wordsArr)
+
+    // printShuffleOrderTesting() // PRENDER SOLO PARA HACER TEST DEL LOOP EN EL FETCH
     // getWordFromApi();   // O N / o f f  of .fetch call and asignment of "cards" place
 
     setSettingBtn.removeEventListener('click', setSetings)
@@ -163,6 +148,9 @@ function setSetings() {
 
 
 const setSettingBtn = document.querySelector('#btnseting')
+
+
+// window.addEventListener('load', setSetings)
 
 window.addEventListener('load', () => {
     setSettingBtn.addEventListener('click', setSetings)
